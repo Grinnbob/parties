8b3d6a2b698ec6b8f4abb414a0e654d081983ea7
brandon <>
2023-08-21 15:44:02 -0700

    fixed onesignal

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by brandon on 8/21/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
