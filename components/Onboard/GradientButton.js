3f81eea42ff94880feac64283217ae7ee07882e3
Asad Siddiqui <asad@nakeddev.com>
2023-08-29 11:59:53 -0700

    linear gradient added for selection

--- components/Onboard/GradientButton.js
+++ components/Onboard/GradientButton.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, Pressable } from "react-native";
+import { StyleSheet, Text, Pressable, View } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
 import { FontFamily, Border, FontSize, Color } from "../../GlobalStyles";
 
@@ -22,17 +22,45 @@ export default ({
       ]}
     >
       <LinearGradient
-        style={[styles.vendor, styles.vendorShadowBox]}
+        style={[
+          styles.vendor,
+          styles.shadowGradient,
+          {
+            paddingVertical: enable ? 3 : 0,
+            paddingHorizontal: enable ? 3 : 0,
+            width: enable ? 333 : 327,
+            borderRadius: 22,
+          },
+        ]}
         locations={[0, 1]}
         colors={
           enable
-            ? ["rgba(108, 27, 158, 0.2)", "rgba(255, 7, 126, 0.2)"]
-            : ["rgba(255, 255, 255, 0.1)", "rgba(255, 255, 255, 0.1)"]
+            ? ["rgba(108, 27, 158, 1)", "rgba(255, 7, 126, 1)"]
+            : ["transparent", "transparent"]
         }
         useAngle={true}
         angle={-90}
       >
-        <Text style={[styles.iamAParty, styles.partyTypo]}>{title}</Text>
+        <View
+          style={{
+            backgroundColor: enable ? "#000" : "transparent",
+            borderRadius: 20,
+          }}
+        >
+          <LinearGradient
+            style={[styles.vendor, styles.vendorShadowBox]}
+            locations={[0, 1]}
+            colors={
+              enable
+                ? ["rgba(108, 27, 158, 0.2)", "rgba(255, 7, 126, 0.2)"]
+                : ["rgba(255, 255, 255, 0.1)", "rgba(255, 255, 255, 0.1)"]
+            }
+            useAngle={true}
+            angle={-90}
+          >
+            <Text style={[styles.iamAParty, styles.partyTypo]}>{title}</Text>
+          </LinearGradient>
+        </View>
       </LinearGradient>
     </Pressable>
   );
@@ -53,7 +81,23 @@ const styles = StyleSheet.create({
       height: 8,
     },
     shadowColor: "rgba(27, 27, 27, 0.16)",
-    borderRadius: Border.br_base,
+    borderRadius: 20,
+    alignItems: "center",
+  },
+  shadowGradient: {
+    justifyContent: "center",
+
+    flexDirection: "row",
+
+    shadowOpacity: 1,
+    elevation: 16,
+    shadowRadius: 16,
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    shadowColor: "rgba(27, 27, 27, 0.16)",
+
     alignItems: "center",
   },
   partyTypo: {
@@ -76,7 +120,7 @@ const styles = StyleSheet.create({
   button: {
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 15,
+    borderRadius: 20,
     marginBottom: 10,
     marginTop: 10,
   },
