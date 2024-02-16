cd5c5aea1fec597e3c53e8f2c62b33303ffb7f19
Alexey Gavrilov <alexey.gavrilov@ontarget-group.com>
2024-02-16 14:30:48 +0300

    close modal on click

--- screens/ServicePackageScreen.tsx
+++ screens/ServicePackageScreen.tsx
@@ -1,37 +1,37 @@
-import React, { useState, useMemo, useCallback } from "react";
-import { Image, StyleSheet, Text, View, ScrollView } from "react-native";
-import { Padding, Border, FontFamily, Color } from "../GlobalStyles";
-import { useToast } from "native-base";
-import apis from "../apis";
-import TopNavigationContent from "../components/TopNavigationContent";
-import ServicePackageModal from "../components/ServicePackageModal";
-import StateTypes from "../stateManagement/StateTypes";
-import useGlobalState from "../stateManagement/hook";
-import { SelectInput } from "../components/Input";
-import { TextInput } from "../components/Input";
-import { TextInputWithAI } from "../components/Moleculs/TextInputWithAI";
-import { PhotoInput } from "../components/Input/PhotoInput";
-import { GradientButton } from "../components/Atoms";
-import { useRecoilState } from "recoil";
-import { serviceTypesAtom, vendorProfileAtom } from "../stateManagement";
-import FastImage from "react-native-fast-image";
+import React, {useState, useMemo, useCallback} from 'react';
+import {Image, StyleSheet, Text, View, ScrollView} from 'react-native';
+import {Padding, Border, FontFamily, Color} from '../GlobalStyles';
+import {useToast} from 'native-base';
+import apis from '../apis';
+import TopNavigationContent from '../components/TopNavigationContent';
+import ServicePackageModal from '../components/ServicePackageModal';
+import StateTypes from '../stateManagement/StateTypes';
+import useGlobalState from '../stateManagement/hook';
+import {SelectInput} from '../components/Input';
+import {TextInput} from '../components/Input';
+import {TextInputWithAI} from '../components/Moleculs/TextInputWithAI';
+import {PhotoInput} from '../components/Input/PhotoInput';
+import {GradientButton} from '../components/Atoms';
+import {useRecoilState} from 'recoil';
+import {serviceTypesAtom, vendorProfileAtom} from '../stateManagement';
+import FastImage from 'react-native-fast-image';
 
-export const ServicePackageScreen = ({ navigation, route }) => {
+export const ServicePackageScreen = ({navigation, route}) => {
   const toast = useToast();
   const service = route.params?.service;
   const [serviceTypes] = useRecoilState(serviceTypesAtom);
-  const [packageName, setPackageName] = useState(service?.name || "");
+  const [packageName, setPackageName] = useState(service?.name || '');
   const [serviceType, setServiceType] = useState(
     serviceTypes.data
-      .find((item) => item.id === service?.serviceTypes?.[0]?.id)
-      ?.id?.toString() || ""
+      .find(item => item.id === service?.serviceTypes?.[0]?.id)
+      ?.id?.toString() || '',
   );
-  const [price, setPrice] = useState(service?.price?.toString() || "");
-  const [rate, setRate] = useState(service?.rate || "");
-  const [description, setDescription] = useState(service?.description || "");
+  const [price, setPrice] = useState(service?.price?.toString() || '');
+  const [rate, setRate] = useState(service?.rate || '');
+  const [description, setDescription] = useState(service?.description || '');
   const [isModalVisible, setModalVisible] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [photo, setPhoto] = useState(service?.image || "");
+  const [photo, setPhoto] = useState(service?.image || '');
   const [isNewPhoto, setIsNewPhoto] = useState(false);
   const [isAiDescriptionLoading, setIsAiDescriptionLoading] = useState(false);
 
@@ -64,13 +64,13 @@ export const ServicePackageScreen = ({ navigation, route }) => {
         res = await apis.service.create(data);
         id = res.data.id;
       }
-      const updatedService = { ...res.data };
+      const updatedService = {...res.data};
       if (isNewPhoto) {
         const newPhoto = await apis.service.uploadServicePhoto({
           id,
           uri: photo,
         });
-        console.log("newPhoto", newPhoto);
+        console.log('newPhoto', newPhoto);
         FastImage.preload([
           {
             uri: newPhoto.data.image,
@@ -81,7 +81,7 @@ export const ServicePackageScreen = ({ navigation, route }) => {
       setIsLoading(false);
       if (res && res.success === false) {
         toast.show({
-          placement: "top",
+          placement: 'top',
           description: res.message,
         });
       }
@@ -90,8 +90,8 @@ export const ServicePackageScreen = ({ navigation, route }) => {
           setModalVisible(true);
         } else {
           toast.show({
-            placement: "top",
-            description: "Service updated",
+            placement: 'top',
+            description: 'Service updated',
           });
           navigation.pop();
         }
@@ -109,22 +109,22 @@ export const ServicePackageScreen = ({ navigation, route }) => {
   const unitOptions = useMemo(() => {
     return [
       {
-        label: "Person",
-        value: "person",
+        label: 'Person',
+        value: 'person',
       },
       {
-        label: "Hour",
-        value: "hour",
+        label: 'Hour',
+        value: 'hour',
       },
       {
-        label: "Day",
-        value: "day",
+        label: 'Day',
+        value: 'day',
       },
     ];
   }, []);
 
   const serviceTypeOptions = useMemo(() => {
-    return serviceTypes.data.map((item) => {
+    return serviceTypes.data.map(item => {
       return {
         label: item.title,
         value: String(item.id),
@@ -136,23 +136,23 @@ export const ServicePackageScreen = ({ navigation, route }) => {
     try {
       if (!packageName || !serviceType) {
         toast.show({
-          placement: "top",
-          description: "Please enter Package Name and Service Type",
+          placement: 'top',
+          description: 'Please enter Package Name and Service Type',
         });
         return;
       }
       setIsAiDescriptionLoading(true);
       const response = await apis.service.generateAiDescription({
         name: packageName,
-        type: serviceTypeOptions.find((item) => item.value === serviceType)
+        type: serviceTypeOptions.find(item => item.value === serviceType)
           ?.label,
       });
       if (response.success && !!response.data.choices?.[0]?.message?.content) {
         setDescription(response.data.choices?.[0]?.message?.content);
       } else {
         toast.show({
-          placement: "top",
-          description: "AI not available now",
+          placement: 'top',
+          description: 'AI not available now',
         });
       }
     } finally {
@@ -166,12 +166,12 @@ export const ServicePackageScreen = ({ navigation, route }) => {
         modalVisible={isModalVisible}
         setModalVisible={setModalVisible}
       />
-      <ScrollView style={{ backgroundColor: Color.labelColorLightPrimary }}>
+      <ScrollView style={{backgroundColor: Color.labelColorLightPrimary}}>
         <View style={styles.servicepackagescreen}>
           <Image
             style={styles.bgIcon}
             resizeMode="cover"
-            source={require("../assets/bg3.png")}
+            source={require('../assets/bg3.png')}
           />
           <TopNavigationContent LeftComponent={() => navigation.pop()} />
           <View style={styles.title}>
@@ -188,8 +188,8 @@ export const ServicePackageScreen = ({ navigation, route }) => {
             <TextInput
               style={[styles.form, styles.formBorder]}
               inputProps={{
-                placeholder: "Package Name",
-                keyboardType: "default",
+                placeholder: 'Package Name',
+                keyboardType: 'default',
                 value: packageName,
                 onChangeText: setPackageName,
                 marginBottom: 1,
@@ -199,7 +199,7 @@ export const ServicePackageScreen = ({ navigation, route }) => {
               selectedValue={serviceType}
               placeholder="Service Type"
               options={serviceTypeOptions}
-              onValueChange={(itemValue) => setServiceType(itemValue)}
+              onValueChange={itemValue => setServiceType(itemValue)}
             />
             <Text style={styles.estimateThisPackage}>
               Estimate This Package Price
@@ -210,9 +210,9 @@ export const ServicePackageScreen = ({ navigation, route }) => {
                 inputProps={{
                   value: price,
                   onChangeText: setPrice,
-                  keyboardType: "phone-pad",
-                  returnKeyType: "next",
-                  placeholder: "--",
+                  keyboardType: 'phone-pad',
+                  returnKeyType: 'next',
+                  placeholder: '--',
                   style: {
                     paddingLeft: 0,
                     paddingRight: 8,
@@ -223,10 +223,9 @@ export const ServicePackageScreen = ({ navigation, route }) => {
                         styles.usdSymbol,
                         {
                           color:
-                            price !== "" ? Color.textMainWhite : Color.gray300,
+                            price !== '' ? Color.textMainWhite : Color.gray300,
                         },
-                      ]}
-                    >
+                      ]}>
                       $
                     </Text>
                   ),
@@ -239,12 +238,12 @@ export const ServicePackageScreen = ({ navigation, route }) => {
               <SelectInput
                 selectedValue={rate}
                 options={unitOptions}
-                onValueChange={(itemValue) => setRate(itemValue)}
+                onValueChange={itemValue => setRate(itemValue)}
                 placeholder="Person/ Hour / Day"
                 width={160}
-                style={{ paddingLeft: 8, paddingRight: 0 }}
+                style={{paddingLeft: 8, paddingRight: 0}}
                 marginBottom={2}
-                arrowIconStyle={{ display: "none" }}
+                arrowIconStyle={{display: 'none'}}
               />
             </View>
             <TextInputWithAI
@@ -252,8 +251,8 @@ export const ServicePackageScreen = ({ navigation, route }) => {
               inputProps={{
                 value: description,
                 onChangeText: setDescription,
-                keyboardType: "default",
-                placeholder: "Service Package Description",
+                keyboardType: 'default',
+                placeholder: 'Service Package Description',
               }}
               isLoading={isAiDescriptionLoading}
               onGeneratePress={generateAiDescription}
@@ -281,41 +280,41 @@ export const ServicePackageScreen = ({ navigation, route }) => {
 const styles = StyleSheet.create({
   packagePosition: {
     marginTop: 10,
-    flexDirection: "column",
+    flexDirection: 'column',
     gap: 16,
   },
   formBorder: {
     paddingVertical: Padding.p_base,
     borderRadius: Border.br_5xs,
     borderWidth: 1,
-    borderColor: "rgba(138, 138, 138, 0.3)",
-    borderStyle: "solid",
-    width: "100%",
+    borderColor: 'rgba(138, 138, 138, 0.3)',
+    borderStyle: 'solid',
+    width: '100%',
     paddingHorizontal: Padding.p_5xl,
   },
   formSpaceBlock1: {
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   formSpaceBlock: {
     padding: Padding.p_base,
     marginLeft: 8,
-    flexDirection: "row",
-    borderStyle: "solid",
+    flexDirection: 'row',
+    borderStyle: 'solid',
     borderRadius: Border.br_5xs,
-    alignItems: "center",
+    alignItems: 'center',
   },
   perTypo: {
     lineHeight: 21,
     fontFamily: FontFamily.typographyBodyMediumLight,
-    fontWeight: "300",
+    fontWeight: '300',
     fontSize: 16,
-    textAlign: "left",
+    textAlign: 'left',
     color: Color.labelColorDarkPrimary,
   },
   header: {
     paddingBottom: Padding.p_4xs,
     paddingTop: Padding.p_4xs,
-    width: "100%",
+    width: '100%',
   },
   backIconLayout: {
     height: 40,
@@ -324,47 +323,47 @@ const styles = StyleSheet.create({
   xIconLayout: {
     height: 24,
     width: 24,
-    overflow: "hidden",
+    overflow: 'hidden',
   },
   bgIcon: {
     top: 0,
     width: 665,
     height: 891,
     left: 0,
-    position: "absolute",
+    position: 'absolute',
   },
   title1: {
     fontSize: 28,
     fontFamily: FontFamily.textLargeBold,
     width: 327,
     color: Color.labelColorDarkPrimary,
-    fontWeight: "700",
-    textAlign: "left",
+    fontWeight: '700',
+    textAlign: 'left',
     marginTop: -16,
   },
   title2: {
     color: Color.gray300,
     marginTop: 8,
     fontFamily: FontFamily.typographyBodyMediumLight,
-    fontWeight: "300",
+    fontWeight: '300',
     lineHeight: 22,
     fontSize: 16,
     width: 327,
-    textAlign: "left",
+    textAlign: 'left',
   },
   title: {
     paddingBottom: Padding.p_5xl,
     paddingTop: 10,
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
-    width: "100%",
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    width: '100%',
   },
   form: {
-    flexDirection: "row",
+    flexDirection: 'row',
     // paddingVertical: Padding.p_base,
     borderRadius: Border.br_5xs,
-    alignItems: "center",
-    color: "#FFF",
+    alignItems: 'center',
+    color: '#FFF',
     marginBottom: 5,
   },
   estimateThisPackage: {
@@ -372,59 +371,59 @@ const styles = StyleSheet.create({
     fontFamily: FontFamily.typographyBodySmallBold,
     fontSize: 16,
     color: Color.labelColorDarkPrimary,
-    fontWeight: "700",
+    fontWeight: '700',
     marginTop: 8,
   },
   form2: {
     marginLeft: 8,
     width: 80,
     borderWidth: 1,
-    borderColor: "rgba(138, 138, 138, 0.3)",
-    color: "#FFF",
+    borderColor: 'rgba(138, 138, 138, 0.3)',
+    color: '#FFF',
   },
   starting: {
-    flexDirection: "row",
-    alignItems: "center",
-    width: "100%",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    justifyContent: 'space-between',
   },
   form4: {
     height: 137,
     paddingVertical: Padding.p_base,
     borderRadius: Border.br_5xs,
     borderWidth: 1,
-    borderColor: "rgba(138, 138, 138, 0.3)",
-    borderStyle: "solid",
-    width: "100%",
+    borderColor: 'rgba(138, 138, 138, 0.3)',
+    borderStyle: 'solid',
+    width: '100%',
     paddingHorizontal: Padding.p_5xl,
     marginTop: 16,
-    color: "#FFF",
+    color: '#FFF',
     paddingTop: 20,
   },
   form5: {
     paddingVertical: Padding.p_base,
     borderRadius: Border.br_5xs,
     borderWidth: 1,
-    borderColor: "rgba(138, 138, 138, 0.3)",
-    borderStyle: "solid",
-    width: "100%",
+    borderColor: 'rgba(138, 138, 138, 0.3)',
+    borderStyle: 'solid',
+    width: '100%',
     paddingHorizontal: Padding.p_5xl,
     height: 54,
     marginTop: 16,
-    alignItems: "center",
-    color: "#FFF",
+    alignItems: 'center',
+    color: '#FFF',
   },
   topnavigationContent: {
-    width: "100%",
+    width: '100%',
     marginTop: 30,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   servicepackagescreen: {
     backgroundColor: Color.labelColorLightPrimary,
-    width: "100%",
-    height: "100%",
-    overflow: "hidden",
+    width: '100%',
+    height: '100%',
+    overflow: 'hidden',
     flex: 1,
     padding: 20,
   },
