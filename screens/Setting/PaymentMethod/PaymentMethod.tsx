fef50b9b3d0accd0c4254750d63cce790cfe0cac
Alexey Gavrilov <alexey.gavrilov@ontarget-group.com>
2024-03-07 16:26:20 +0300

    added setup stripe account for vendor

--- screens/Setting/PaymentMethod/PaymentMethod.tsx
+++ screens/Setting/PaymentMethod/PaymentMethod.tsx
@@ -1,29 +1,44 @@
-import React, {useEffect, useState} from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState, useCallback} from 'react';
+import {View, Linking} from 'react-native';
 import {Spinner} from 'native-base';
 import {WebView} from 'react-native-webview';
-import Config from 'react-native-config';
 import apis from '../../../apis';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import {Color} from '../../../GlobalStyles';
 import TopNavigationContent from '../../../components/TopNavigationContent';
 import {useNavigation} from '@react-navigation/core';
-
+import {Color} from '../../../GlobalStyles';
+import {useFocusEffect} from '@react-navigation/native';
+import layout from '../../../utils/layout';
 export const PaymentMethod: React.FC = () => {
   const [loading, setLoading] = useState(true);
-  const insets = useSafeAreaInsets();
+  const [url, setUrl] = useState('');
   const navigation = useNavigation();
 
   useEffect(() => {
-    apis.user
-      .onboardStart()
-      .then(response => {
-        console.log('response', response);
-      })
-      .catch(e => {
-        console.log('e', e);
-      });
+    Linking.addEventListener('url', event => {
+      if (
+        event.url.includes(
+          'payments/vendor/onboard-fail' || 'payments/vendor/onboard-success',
+        )
+      ) {
+        setUrl('');
+        setLoading(true);
+        apis.user.onboardStart().then(response => {
+          setUrl(response.data.url);
+        });
+      }
+    });
   }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      setUrl('');
+      setLoading(true);
+      apis.user.onboardStart().then(response => {
+        console.log('response', response);
+        setUrl(response.data.url);
+      });
+    }, []),
+  );
 
   return (
     <View
@@ -42,21 +57,28 @@ export const PaymentMethod: React.FC = () => {
           style={{
             justifyContent: 'center',
             alignItems: 'center',
-            height: '100%',
             zIndex: 1,
+            flex: 1,
+            paddingBottom: 40,
+            position: 'absolute',
+            bottom: (layout.window.height - 150) / 2,
+            left: (layout.window.width - 20) / 2,
           }}>
-          <Spinner color="#0C1D37" />
+          <Spinner color={Color.primaryPink} size="lg" />
         </View>
       )}
-      <WebView
-        source={{uri: Config.PRIVACY_LINK}}
-        onLoadProgress={({nativeEvent}) => {
-          this.loadingProgress = nativeEvent.progress;
-          if (nativeEvent.progress === 1) {
-            setLoading(false);
-          }
-        }}
-      />
+      {!!url && (
+        <WebView
+          style={{backgroundColor: Color.labelColorLightPrimary}}
+          source={{uri: url}}
+          onLoadProgress={({nativeEvent}) => {
+            this.loadingProgress = nativeEvent.progress;
+            if (nativeEvent.progress === 1) {
+              setLoading(false);
+            }
+          }}
+        />
+      )}
     </View>
   );
 };
